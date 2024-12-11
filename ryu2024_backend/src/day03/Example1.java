30061a5e070809fa8f0ae41b500ce0c64376dc68
Ryuyena0305 <Ryuyena0305@gmail.com>
2024-12-11 11:34:42 +0900

    day23 2교시

--- ryu2024_backend/src/day03/Example1.java
+++ ryu2024_backend/src/day03/Example1.java
@@ -39,14 +39,76 @@ public class Example1 { //클래스 시작
 		double avg = total /2.0;//평균구하기
 		System.out.println(avg);//평균변수의 값 출력한다.
 		
-		//[1분복습]
-		int mathScore2 = 10;
-		int engScore2 = 20;
-		int korScore2 = 30;
-		//수학점수와 영어점수 값을 더해서 총점을 의미하는 변수에 대입한다. 
-		int totalScore2 = korScore2+mathScore2 + engScore2;
-		double avgScore2 = totalScore2 /3.0;
-		System.out.println(avgScore2);
+			//[1분복습]
+			int mathScore2 = 10;
+			int engScore2 = 20;
+			int korScore2 = 30;
+			//수학점수와 영어점수 값을 더해서 총점을 의미하는 변수에 대입한다. 
+			int totalScore2 = korScore2+mathScore2 + engScore2;
+			double avgScore2 = totalScore2 /3.0;
+			System.out.println(avgScore2);
+			
+		//[4] 증감연산자 ,, ++ --
+		int num2 = 10;
+		int value2;
+		value2 = ++num2; //num2값이 1증가 한 후에 value2 변수에 대입한다.
+		System.out.println(value2); //value2 = 11, num2 = 11
+		value2 = num2++; //value2 변수에 기존 num2값이 먼저 대입한 후에 1증가한다.
+		System.out.println(value2);//value2 = 11, num2 = 12
+		value2 = --num2; 	//num2 값이 1감소한 후에 value2 변수에 대입한다.
+		System.out.println(value2);//value2 = 11, num2 = 11
+		value2 = num2--;//value2변수에 기존 num2 값이 먼저 대입한 후에 num2값이 1감소한다.
+		System.out.println(value2);//value2 = 11, num2 = 10
+		
+		int gameScore = 150;
+		int lastScore = ++gameScore;//변수앞에 ++가 있으므로 대입(=)보다 1증가가 우선이다.
+		System.out.println(lastScore);//151
+		
+		int lastScore2 = --gameScore;//변수앞에 ++가 있으므로 대입(=)보다 1증가가 우선이다.
+		System.out.println(lastScore2);//150
+		
+		// *사칙연산의 우선순위에 따라 *곱하기 한 후 +더하기 -> 0
+		int result1 = 3+3*2;System.out.println(result1);
+		
+		int x =10;
+		int y = x-- + 5+ --x;
+		// - 연산순서
+		//1. x--+5 --> 10 + 5 ->15 [x는 +연산후 1차감] x= 9
+		//1. 15 + --x --> 15 + 8 ->23 [x는 +연산전 1차감] x= 8
+		
+		System.out.println(x);//8
+		System.out.println(y);//23
+		
+		//[5]비교연산자,  >초과 < 미만 >=이상 <=이하 ==같다 !=다르다 , 결과는 true or false
+		int myAge = 24;
+		boolean value = (myAge >25);
+		System.out.println(value);//false // () 소괄호를 사용한 이유는 가독성 높이기 위해서
+		
+		//[6]논리연산자, &&논리곱 : 모두 참이면 참, ||논리합 !부정  : 하나라도 참이면 참, !부정
+		int num1 = 10;
+		int i = 2;
+		
+		boolean value3 = ((num1 = num1 + 10)<10)&& ((i = i+2)<10);
+						//여기서 false이므로, 뒤 조건은 볼 필요 없으므로 코드 실행되지 않는다.
+		System.out.println(value3);	//false
+		System.out.println(num1);		//20
+		System.out.println(i);			//2
+
+		value3 = ((num1 = num1 + 10)<10)|| ((i = i+2)<10);
+		System.out.println(value3);	//true
+		System.out.println(num1);		//30
+		System.out.println(i);			//4
+		
+		//[7]복합 대입 연산자, +=, -=, *=, /=, %=
+		
+		//p.81 [1분 복습]
+		//num1 = num1+5; num1+=5;
+		//num1 = num1*num2; num1*=num2;
+		
+		//[8]조건 연산자, 조건식? 참결과 : 거짓결과
+		int num3 = 5>3?10:20;
+		System.out.println(num3);
+		
 	}
 
 }
