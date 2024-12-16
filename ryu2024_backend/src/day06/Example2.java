334dc119f183d83a6587de3fa3a7a53d109e3996
Ryuyena0305 <Ryuyena0305@gmail.com>
2024-12-16 12:59:05 +0900

    day26 4교시

---
+++ ryu2024_backend/src/day06/Example2.java
@@ -0,0 +1,65 @@
+package day06;
+
+import java.util.Scanner;
+
+/*자바 과제4 : 키오스크 프로그램
+ 	- 문법 : 변수,입출력함수,연산자 제어문(조건문/반복문)
+ 	- 제품은 콜라,사이다,환타 인 3개가 존재한다는 가정
+ 	- 각 제품의 재고는 초기에 10개씩 존재한다.
+ 	- 각 제품의 가격은 콜라 1000원, 사이다1500원, 환타 2000원 가정한다.
+ 	- 제품을 선택시 제품 재고 1 차감하고 장바구니에 담는다.
+ 	- 메뉴 ] 1.콜라 2.사이다 3.환타 4. 결제
+ 	 	1. 선택시 : 콜라 재고 1 차감하고 장바구니에 담는다.
+ 	 	2. 선택시 : 사이다 재고 1차감하고 장바구니에 담는다.
+ 	 	3. 선택시 : 환타 재고 1차감하고 장바구니에 담는다.
+ 	 	4. 선택시 현재 장바구니에 담긴 모든 현황을 보여주고 금액 합산을 출력한다.
+ 	 		ex) 콜라1개, 사이다2개, 환타 0개 총 금액 : 4000원
+ 	 	*추가 유효성 검사 : 만일 재고가 부족하면 "재고가 부족합니다."라고 출력하시오.
+ */
+public class Example2 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int colaNum = 10;
+		int ciderNum = 10;
+		int fantaNum = 10;
+		int colaPrice = 1000;
+		int ciderPrice = 1500;
+		int fantaPrice = 2000;
+		String cart = null;
+
+		while (true) {
+			System.out.println(" 장바구니에 담을 재고를 선택하거나 결제하세요 1.콜라 2.사이다 3.환타 4. 결제.");
+			int selectNum = scan.nextInt();
+		
+			if (selectNum == 1) {
+				if (colaNum == 0) {
+					System.out.println("재고가 부족합니다.");
+				} else {
+					colaNum = colaNum-1;
+				}
+			} else if (selectNum == 2) {
+				if (ciderNum == 0) {
+					System.out.println("재고가 부족합니다.");
+				} else {
+					ciderNum = ciderNum-1;
+				}
+
+			} else if (selectNum == 3) {
+				if (fantaNum == 0) {
+					System.out.println("재고가 부족합니다.");
+				} else {
+					fantaNum = fantaNum-1;
+
+				}
+
+			} else if (selectNum == 4) {
+				System.out.printf("콜라%d개, 사이다%d개, 환타 %d개 총 금액 : %d원", 10 - colaNum, 10 - ciderNum, 10 - fantaNum,
+						(10 - colaNum) * 1000 + (10 - ciderNum) * 1500 + (10 - fantaNum) * 2000);
+
+			}
+
+		}
+	}
+
+}
