b7d70b1e36bf44203b2ee5bbd10a8933e97a583c
Ryuyena0305 <Ryuyena0305@gmail.com>
2024-12-16 16:08:56 +0900

    day26 6교시

---
+++ ryu2024_backend/src/day06/Car.java
@@ -0,0 +1,8 @@
+package day06;
+
+public class Car {
+
+	int 차량번호;
+	String 차량주;
+	
+}
--- ryu2024_backend/src/day06/Example3.java
+++ ryu2024_backend/src/day06/Example3.java
@@ -3,6 +3,7 @@ package day06;
 public class Example3 {
 
 	public static void main(String[] args) {
+		//생활속에 객체들을 클래스 코드로 표현하기
 
 
 	}
