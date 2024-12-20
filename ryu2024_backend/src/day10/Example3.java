cbcf50169c054f4d592802bbaf8b80f753eeb1a9
Ryuyena0305 <Ryuyena0305@gmail.com>
2024-12-20 13:16:45 +0900

    day30 4교시

---
+++ ryu2024_backend/src/day10/Example3.java
@@ -0,0 +1,19 @@
+package day10;
+
+public class Example3 {
+	public static void main(String[] args) {
+		//[1] Dto 객체 생성하기
+		BoardDto board = new BoardDto();
+		// - title 멤버변수는 private 이므로 오류 발생
+		//board.title = "안녕하세요";
+		board.setTitle("안녕하세요.");
+		
+		//- title 멤버변수는 private 이므로 오류 발생
+		//System.out.println(board.title);
+		System.out.println(board.getTitle());
+		
+	System.out.println(board);
+	}
+	
+
+}
