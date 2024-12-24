af1ca1e1a43f852f66d10231707b8b4421a0dff2
Ryuyena0305 <Ryuyena0305@gmail.com>
2024-12-24 14:22:46 +0900

    day32 javaday10 9 오류수정

--- ryu2024_backend/src/day09/LoginFunction.java
+++ ryu2024_backend/src/day09/LoginFunction.java
@@ -4,52 +4,54 @@ import java.util.Scanner;
 
 public class LoginFunction {
 	Scanner scan = new Scanner(System.in);
+	UserInfo[] userInfoArray = new UserInfo[100];
 
-	void userInfoAdd(UserInfo[] userInfoArray){
+	void userInfoAdd(UserInfo[] userInfoArray) {
 		System.out.println(">>회원가입을 선택했습니다.");
 		System.out.println(">아이디 : ");
-		String id = scan.next(); 
+		String id = scan.next();
 		System.out.println(">비밀번호 : ");
 		String pw = scan.next();
 		System.out.println(">닉네임 : ");
 		String nickname = scan.next();
-		
+
 		UserInfo userInfo = new UserInfo();
 		userInfo.id = id;
 		userInfo.pw = pw;
-		userInfo.nickname= nickname;}
+		userInfo.nickname = nickname;
 
-		boolean saveState= false;
-		for(int index=0;index<userInfoArray.length-1;index++) {
-			if(userInfoArray[index]==null) {
-				userInfoArray[index]=userInfo;
+		boolean saveState = false;
+		for (int index = 0; index < userInfoArray.length - 1; index++) {
+			if (userInfoArray[index] == null) {
+				userInfoArray[index] = userInfo;
 				saveState = true;
 				break;
 			}
 		}
-		if(saveState) {System.out.println("등록 성공하였습니다.");}	
-		else {
+		if (saveState) {
+			System.out.println("등록 성공하였습니다.");
+		} else {
 			System.out.println("등록 실패하였습니다.");
 		}
 	}
+
 	void userInfoLogin(UserInfo[] userInfos) {
 		System.out.println(">>로그인을 선택했습니다.");
 		System.out.println(">아이디 : ");
-		String checkId = scan.next(); 
+		String checkId = scan.next();
 		System.out.println(">패스워드 : ");
 		String checkPw = scan.next();
-		
-		for(int index=0; index<=userInfos.length-1;index++) {
-			if( userInfos[index].id!=null&& userInfos[index].id.equals(checkId) && userInfos[index].pw!=null&&userInfos[index].pw.equals(checkPw)){
+
+		for (int index = 0; index <= userInfos.length - 1; index++) {
+			if (userInfos[index].id != null && userInfos[index].id.equals(checkId) && userInfos[index].pw != null
+					&& userInfos[index].pw.equals(checkPw)) {
 				System.out.println("로그인 성공");
 				break;
-			}else {
+			} else {
 				System.out.println("로그인 실패");
 			}
 		}
-	
-	
-	
+
 	}
-	
+
 }
