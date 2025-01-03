8c3529bcb67c4dfd7dee8701570b3b1ca922687e
Ryuyena0305 <Ryuyena0305@gmail.com>
2025-01-03 14:25:16 +0900

    day 38 4교시 JDBC 정리파일 업로드

--- ryu2024_backend/src/day18/step2/Example1.java
+++ ryu2024_backend/src/day18/step2/Example1.java
@@ -1,11 +1,15 @@
 package day18.step2;
 
+import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Example1 {
 
 	public static void main(String[] args) {
+		
+		//인터페이스 타입
+		Connection connection;
 		// [1] DB 연동
 		//1. mysql JDBC 클래스 존재하는지 로드/불러오기, Class.forName() : 클래스 로드 함수, 일반 예외처리
 		try {
@@ -14,6 +18,7 @@ public class Example1 {
 			//mysql : com.mysql.cj.jdbc.Driver
 			//oracle : oracle.jdbc.driver.OracleDriver
 		//2. db서버와 연동 시도
+		connection =
 		DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb0103","root","1234");
 			//DriverManager : DB서버와 연동하는 클래스 , import java.sql
 				//->연동 함수 : .getConnection("jdbc:mysql://DB서버IP:DB서버PORT/DB명","root","1234");
